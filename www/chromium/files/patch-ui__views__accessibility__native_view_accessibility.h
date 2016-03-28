2df5ecd7b8c64a5cab35efb90b82af8e9a25e471
Christoph Moench-Tegeder <cmt@burggraben.net>
2016-03-28 18:43:43 +0200

    final fixes

--- www/chromium/files/patch-ui__views__accessibility__native_view_accessibility.h
+++ www/chromium/files/patch-ui__views__accessibility__native_view_accessibility.h
@@ -5,7 +5,7 @@
  #endif
  
 -#if defined(OS_LINUX) && defined(USE_X11) && !defined(OS_CHROMEOS)
-+#if defined(OS_LINUX) && defined(USE_X11) && !defined(OS_CHROMEOS) && !defined(OS_BSD)
++#if (defined(OS_LINUX) || defined(OS_BSD)) && defined(USE_X11) && !defined(OS_CHROMEOS)
  #define PLATFORM_HAS_NATIVE_VIEW_ACCESSIBILITY_IMPL 1
  #endif
  
